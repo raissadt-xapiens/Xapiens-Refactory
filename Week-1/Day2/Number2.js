a74c6d2e22099fc26cb39b454190a415eb4b774f
Raissa Desyandita <Raissa.Desyandita@xapiens.id>
2021-01-19 15:46:27 +0700

    Upload Num2 [Day2]

---
+++ Week-1/Day2/Number2.js
@@ -0,0 +1,16 @@
+// Even Number
+var x = [];
+for (x = 0; x <= 20; x++) {
+    if (x % 2 == 0) {
+        console.log(x);
+    }
+}
+
+
+// Odd Number
+var y = [];
+for (y = 0; y <= 20; y++) {
+    if (y % 2 == 1) {
+        console.log(y);
+    }
+}
