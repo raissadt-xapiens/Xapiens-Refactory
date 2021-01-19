6610733545151749aee82d5186f6e798e1b078c4
Raissa Desyandita <raissadesyandita@gmail.com>
2021-01-19 20:12:23 +0700

    Update Num6 [Day2]

--- Week-1/Day2/Number_6.js
+++ Week-1/Day2/Number_6.js
@@ -3,3 +3,25 @@ numbers.sort(function(a, b) {
     return a - b;
 })
 console.log(numbers);
+
+numbers.sort(function(a, b) {
+    return b - a;
+})
+console.log(numbers);
+
+
+function chunkArray(myArray, chunk_size) {
+    var index = 0;
+    var arrayLength = myArray.length;
+    var tempArray = [];
+
+    for (index = 0; index < arrayLength; index += chunk_size) {
+        myChunk = myArray.slice(index, index+chunk_size);
+        tempArray.push(myChunk);
+    }
+
+    return tempArray;
+}
+
+var result = chunkArray([89, 77, 75, 66, 55, 44, 44, 44, 33, 33, 33, 22, 9, 8, 7, 6, 5, 4, 4, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 1, 1, 1], 11);
+console.log(result);
