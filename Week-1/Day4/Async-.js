4c42d672d8faaafeabb2c1bde189d335eee9a073
Raissa Desyandita <Raissa.Desyandita@xapiens.id>
2021-01-21 16:49:24 +0700

    Update Async [Day4]

--- Week-1/Day4/Async-.js
+++ Week-1/Day4/Async-.js
@@ -1,24 +1,12 @@
-/* Promise */
 for (let i = 1; i <= 3; i++) {
     setTimeout(() => {
         console.log(i)
     }, 1000);
 }
 
-console.log('Done');
-
-
-
-/* Await */
-const i = [];
-const forLoop = async => {
-    console.log('Start');
-
-    for (let i = 1; i <= 3; i++) {
-        const number = numberToGet[i]
-        const numRan = await getnumRan(number)
-        console.log(numRan)
-    }
-
-    console.log('End');
+function setDelay(x) {
+    setTimeout(function() {
+        console.log('Done');
+    }, 1500);
 }
+setDelay();
