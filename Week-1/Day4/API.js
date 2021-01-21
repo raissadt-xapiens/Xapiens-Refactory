7bc7e4c58c328fb1268a8139756a6e7304dd6c4b
Raissa Desyandita <Raissa.Desyandita@xapiens.id>
2021-01-21 15:57:58 +0700

    Update RestAPI [Day4]

--- Week-1/Day4/API.js
+++ Week-1/Day4/API.js
@@ -1,29 +1,21 @@
-const url = `https://jsonplaceholder.typicode.com/posts/${userId}`;
-axios.get(url)
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-
-
-
-
 const axios = require('axios');
 const users = [];
 
 function getUrl() {
     axios
-        .get("https://jsonplaceholder.typicode.com/post/")
+        .get("https://jsonplaceholder.typicode.com/posts?userId=1")
         .then(res => console.log(res))
         // .catch(err => console.log(err))
 }
 
 function byID() {
     axios
-        .get("https://jsonplaceholder.typicode.com/users/")
+        .get("https://jsonplaceholder.typicode.com/users?userId=1")
         .then(res => console.log(res))
         // .catch(err => console.log(err))
 }
 
-const data = axios.all([getUrl(byID())])
+const data = axios.all([getUrl(), byID()])
 
 users.push(data);
 console.log(users);
