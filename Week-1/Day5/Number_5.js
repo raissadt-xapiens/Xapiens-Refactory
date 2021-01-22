faed38a76e9d455444e3af9f66a4035ece3a652a
Raissa Desyandita <Raissa.Desyandita@xapiens.id>
2021-01-22 14:48:37 +0700

    Update Num5 [Day5]

--- Week-1/Day5/Number_5.js
+++ Week-1/Day5/Number_5.js
@@ -1,9 +1,3 @@
 var str = prompt("Input value");
-console.log(str + str.split(/\s/).reverse().join(" "));
-
-
-var str = "saya ingin makan nasi goreng";
-var arr = str.split(" ");
-arr.reverse();
-str = arr.join(" ");
-console.log(str + arr.join(" "))
+var result = str.split("").reverse().join("").split(" ").reverse().join(" ");
+console.log(str.concat(result));
