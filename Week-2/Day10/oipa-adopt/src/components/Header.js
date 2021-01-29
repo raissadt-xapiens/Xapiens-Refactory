99581b633a44b42e0c2ce112e1ad87c41628f730
Raissa Desyandita <Raissa.Desyandita@xapiens.id>
2021-01-29 13:00:33 +0700

    Add template for Introduction [Day10]

--- Week-2/Day10/oipa-adopt/src/components/Header.js
+++ Week-2/Day10/oipa-adopt/src/components/Header.js
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap';
+import '../assets/styles/Header.css';
 
-const Header = () => {
+const Header = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
     return (
         <div>
-            <h1>Aaaaa</h1>
+            <Navbar
+                className="navigation"
+                lightexpand="lg">
+                <NavbarBrand
+                    className="mr-auto"
+                >
+                    <h2
+                        style={{
+                            fontStyle: "italic",
+                            marginLeft: "10px"
+                        }}
+                    >
+                        OIPA
+                    </h2>
+                </NavbarBrand>
+                <NavbarToggler
+                    onClick={toggle}
+                />
+                <Collapse
+                    isOpen={isOpen} navbar>
+                    <Nav
+                        className="ml-auto"
+                        navbar
+                        // style={{marginRight: "10px"}}
+                    >
+                        <NavItem>
+                            <NavLink
+                                className="main-link"
+                                href="#"
+                            >
+                                About Us
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink
+                                className="main-link"
+                                href="#"
+                            >
+                                Projects
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink
+                                className="main-link"
+                                href="#"
+                            >
+                                News
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
         </div>
     )
 }
--- Week-2/Day10/oipa-adopt/src/components/Introduction.js
+++ Week-2/Day10/oipa-adopt/src/components/Introduction.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Slider1 from '../assets/images/adoption.jpeg';
 
 function Introduction() {
     return (
         <div>
-            nav
+            <img
+                src={Slider1}
+                alt="Slide 1"
+                style={{
+                width: "100%"
+            }}
+            />
         </div>
     )
 }
