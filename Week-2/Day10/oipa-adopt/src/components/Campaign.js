b7316e79a8e1b964910b87f12ee27a675efe384f
Raissa Desyandita <Raissa.Desyandita@xapiens.id>
2021-01-29 14:25:31 +0700

    Add template for Campaign [Day10]

--- Week-2/Day10/oipa-adopt/src/components/Campaign.js
+++ Week-2/Day10/oipa-adopt/src/components/Campaign.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import {
+    Container,
+    Button
+} from 'reactstrap';
+import '../assets/styles/Content.css';
 
 function Campaign() {
     return (
         <div>
-            join
+            <Container
+                className="themed-container"
+                fluid={true}
+                style={{
+                    height: "650px",
+                    backgroundColor: "white"
+                }}
+            >
+                <h1
+                    className="project"
+                >
+                    Join Our Journey with Many Projects
+                </h1>
+                <hr 
+                    className="line"
+                    style={{
+                        borderColor: "10px solid black"
+                    }}
+                />
+                <br/>
+                <h1
+                    className="project"
+                >
+                    or
+                </h1>
+                <br/>
+                <h1
+                    className="project"
+                >
+                    Help the OIPA Put a Stop to Animal Cruelty
+                </h1>
+                <Button
+                    className="donation"
+                    onCLick="#"
+                >
+                    DONATE
+                </Button>
+            </Container>
         </div>
     )
 }
