c64d57fea9142f8512d0b737550c70c4571e3e48
Raissa Desyandita <Raissa.Desyandita@xapiens.id>
2021-02-02 13:04:19 +0700

    Add Sign components [Day11]

---
+++ Week-3/Day11/developing-web/src/SignIn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Sign.css';
+
+function Signin() {
+    return (
+        <div>
+            <h2>Sign In</h2>
+        </div>
+    )
+}
+
+export default Signin;
--- Week-3/Day11/developing-web/src/index.js
+++ Week-3/Day11/developing-web/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import SignIn from './SignIn';
+// import SignUp from './SignUp';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* <SignIn />
+    <SignUp /> */}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
