19dc24d55439303080c9bfe137b11a266adb2d13
Raissa Desyandita <Raissa.Desyandita@xapiens.id>
2021-02-03 10:24:34 +0700

    Edit style [Day12]

--- Week-3/Day12/developing-web/src/index.js
+++ Week-3/Day12/developing-web/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import SignIn from './SignIn';
-// import SignUp from './SignUp';
-import App from './App';
+import SignUp from './SignUp';
+// import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <SignIn /> */}
-    {/* <SignUp /> */}
-    <App />
+    <SignUp />
+    {/* <App /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
