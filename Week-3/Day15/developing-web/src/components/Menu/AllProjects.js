a3383262b1c7136cd0d6da24b823e1da27de8c1c
Raissa Desyandita <Raissa.Desyandita@xapiens.id>
2021-02-07 00:33:14 +0700

    Add components for Projects Page [Day15]

--- Week-3/Day15/developing-web/src/components/Menu/AllProjects.js
+++ Week-3/Day15/developing-web/src/components/Menu/AllProjects.js
@@ -1,32 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Container,
-    // Carousel,
-    // CarouselItem,
-    // CarouselControl,
-    // CarouselIndicators,
+    Row,
+    Col,
+    Card,
+    Carousel,
+    CarouselItem,
+    CarouselControl,
+    CarouselIndicators,
     // CarouselCaption,
-    // Card,
-    // CardDeck,
-    // CardImg,
-    // CardBody,
-    // CardTitle,
-    // CardSubtitle,
-    // Button
+    ListGroup,
+    ListGroupItem,
+    Button
 } from 'reactstrap';
-import Campaign1 from '../../assets/images/campaign1.jpeg';
+import Projects1 from '../../assets/images/projects1.jpg';
+import Projects2 from '../../assets/images/projects2.jpg';
+import Projects3 from '../../assets/images/projects3.jpg';
 import '../../assets/styles/Menu.css';
 
 
-// const items = [
-//     {
-//         src: {Campaign1}
-//     }
-// ]
+const items = [
+    {
+        src: Projects1,
+        altText: 'Project 1',
+        caption: 'Project 1'
+    },
+    {
+        src: Projects2,
+        altText: 'Project 2',
+        caption: 'Project 2'
+    },
+    {
+        src: Projects3,
+        altText: 'Project 3',
+        caption: 'Project 3'
+    }
+];
 
 
 
 function AllProjects(props) {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [animating, setAnimating] = useState(false);
+
+
+    const next = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+        setActiveIndex(nextIndex);
+    }
+    
+    const previous = () => {
+        if (animating) return;
+        const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+        setActiveIndex(nextIndex);
+    }
+    
+    const goToIndex = (newIndex) => {
+        if (animating) return;
+        setActiveIndex(newIndex);
+    }
+
+    const slides = items.map((item) => {
+        return (
+          <CarouselItem
+            onExiting={() => setAnimating(true)}
+            onExited={() => setAnimating(false)}
+            key={item.src}
+          >
+            <img src={item.src} alt={item.altText} 
+            style={{height: "500px", width: "auto"}} 
+            />
+            {/* <CarouselCaption captionText={item.caption} captionHeader={item.caption} /> */}
+          </CarouselItem>
+        );
+    });
+
     return (
         <div>
             <Container
@@ -39,47 +88,45 @@ function AllProjects(props) {
                 >
                     Campaigns & Projects
                 </h1>
-
-
-
-
-                    {/* <CardDeck
-                        className="deck-of-news"
-                    >
-                        <Card>
-                            <CardImg top width="100%" src={News1} alt="News-1" />
-                                <CardBody>
-                                    <CardTitle tag="h4">News 1</CardTitle>
-                                    <CardSubtitle tag="h5" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-                                    <CardText>Car strayyyyyssssssssssssssssss.</CardText>
-                                    <Button>
-                                        Read more...
-                                    </Button>
-                                </CardBody>
-                        </Card>
-                        <Card>
-                            <CardImg top width="100%" src={News2} alt="News-1" />
-                                <CardBody>
-                                    <CardTitle tag="h4">News 1</CardTitle>
-                                    <CardSubtitle tag="h5" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-                                    <CardText>Car strayyyyyssssssssssssssssss.</CardText>
-                                    <Button>
-                                        Read more...
-                                    </Button>
-                                </CardBody>
-                        </Card>
-                        <Card>
-                            <CardImg top width="100%" src={News1} alt="News-1" />
-                                <CardBody>
-                                    <CardTitle tag="h4">News 1</CardTitle>
-                                    <CardSubtitle tag="h5" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-                                    <CardText>Car strayyyyyssssssssssssssssss.</CardText>
-                                    <Button>
-                                        Read more...
-                                    </Button>
-                                </CardBody>
-                        </Card>
-                    </CardDeck> */}
+                <Card
+                    className="carousel-projects"
+                >
+                <Carousel
+                    activeIndex={activeIndex}
+                    next={next}
+                    previous={previous}
+                >
+                    <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+                    {slides}
+                    <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+                    <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+                </Carousel>
+                </Card>
+                <br/>
+                <Button className="button-support">BECOME A VOLUNTEER</Button>
+                <Row>
+                    <Col>
+                        <h1
+                            className="proj-camp"
+                        >
+                            Issues
+                        </h1>
+                        <ListGroup
+                            className="list-issues"
+                        >
+                            <ListGroupItem>Abondonment and Straying</ListGroupItem>
+                            <ListGroupItem>Bullfight and Festivals</ListGroupItem>
+                            <ListGroupItem>Circus</ListGroupItem>
+                            <ListGroupItem>Cosmetics</ListGroupItem>
+                            <ListGroupItem>Folk Parties</ListGroupItem>
+                            <ListGroupItem>Hunting</ListGroupItem>
+                            <ListGroupItem>Vivisection</ListGroupItem>
+                        </ListGroup>
+                    </Col>
+                </Row>
+                <br/>
+                <Button className="button-support">BECOME A SUPPORTER</Button>
+                <br/><br/><br/><br/><br/>
             </Container>
         </div>
     )
