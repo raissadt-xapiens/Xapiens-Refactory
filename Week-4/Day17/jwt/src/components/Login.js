ec5d1a4ffdd5b653eca5fa563e035c4769010f50
Raissa Desyandita <Raissa.Desyandita@xapiens.id>
2021-02-09 21:59:42 +0700

    Edit style for form [Day17]

--- Week-4/Day17/jwt/src/components/Login.js
+++ Week-4/Day17/jwt/src/components/Login.js
@@ -28,21 +28,25 @@ export default function Login({ setToken }) {
 
     return (
         <div className="login-wrapper">
-            <h1>Please Log In</h1>
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        <p>Username</p>
-                        <input type="text" onChange={e => setUsername(e.target.value)} />
-                    </label>
-                    <label>
-                        <p>Password</p>
-                        <input type="password" onChange={e => setPassword(e.target.value)} />
-                    </label>
-                    <div>
+            <div className="container">
+                <h1>Please Log In</h1>
+                <br/>
+                    <form onSubmit={handleSubmit}>
+                        <label>
+                            <p>Username</p>
+                            <input type="text" onChange={e => setUsername(e.target.value)} />
+                        </label>
                         <br/>
-                        <button type="submit">Submit</button>
-                    </div>
-                </form>
+                        <label>
+                            <p>Password</p>
+                            <input type="password" onChange={e => setPassword(e.target.value)} />
+                        </label>
+                        <div>
+                            <br/>
+                            <button type="submit">Submit</button>
+                        </div>
+                    </form>
+            </div>
         </div>
     )
 }
